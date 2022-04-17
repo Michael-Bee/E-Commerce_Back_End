6cc11d9c1bd781101738bff950e850ed0837a67f
Michael Bee <bee.michael.02@gmail.com>
2022-04-17 13:53:48 -0500

    Started work on models - Category one possibly done.

--- models/Category.js
+++ models/Category.js
@@ -7,21 +7,24 @@ class Category extends Model {}
 Category.init(
   {
     // id
-        
+    id: {    
         // Integer.
-        
+      type: DataTypes.INTEGER,
         // Doesn't allow null values.
-        
+      allowNull: false,
         // Set as primary key.
-        
+      primaryKey: true,
         // Uses auto increment.
+      autoIncrement: true,
+    },
     
     // category_name
-    
+    category_name: {
         // String.
-        
+      type: DataTypes.STRING,
         // Doesn't allow null values.
-
+      allowNull: false,
+    },
   },
   {
     sequelize,
